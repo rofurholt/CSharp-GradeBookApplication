8a8023a7037e563d982fe9c027c435837153c2aa
rofurholt <42466841+rofurholt@users.noreply.github.com>
2019-03-15 10:04:28 +0100

    Completed the module

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -13,6 +13,7 @@ namespace GradeBook.GradeBooks
     {
         public string Name { get; set; }
         public List<Student> Students { get; set; }
+        public GradeBookType Type { get; set; } //new property
 
         public BaseGradeBook(string name)
         {
