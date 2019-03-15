bea8925461899fe466f2e881f5bdcb488aa375c8
rofurholt <42466841+rofurholt@users.noreply.github.com>
2019-03-15 10:11:20 +0100

    New enum
    complete

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,15 @@
+﻿
+
+using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook //to inherit the BaseGradeBook class
+    {
+        //make a new constructor
+        public StandardGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Standard;
+        }
+    }
+}
