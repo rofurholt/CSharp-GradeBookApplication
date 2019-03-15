435d2d079de5c61619952fd1638f7076902e0602
rofurholt <42466841+rofurholt@users.noreply.github.com>
2019-03-15 11:31:13 +0100

    Completed the module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,6 +1,8 @@
 ﻿
 
 using GradeBook.Enums;
+using System;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -9,6 +11,28 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name) //to create the call to the base constructor
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException("Ranked grading requires at least 5 students." );
+
+            var threshold = (int)Math.Ceiling(Students.Count * 0.2); //how many students must score lower than before I drop the lettergrade * 0.2 will give the 20% of the total nr of studs
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e=>e.AverageGrade).ToList(); //order students by the average and selecting only those and create a list
+
+            if (grades[threshold - 1] <= averageGrade)
+                return 'A';
+            else if (grades[(threshold * 2) - 1] <= averageGrade)
+                return 'B';
+            else if (grades[(threshold * 3) - 1] <= averageGrade)
+                return 'C';
+            else if (grades[(threshold * 4) - 1] <= averageGrade)
+                return 'D';
+            else
+                return 'F';
+
+            
         }
     }
 }
